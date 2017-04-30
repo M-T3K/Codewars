4d6b94f42ab79c324bca70d6bd88ec1623d0e7b4
M-T3K <kevinalb12@gmail.com>
2017-04-30 23:07:30 +0100

    Create BoolToWord.java

---
+++ 8-kyu/Boolean To String/BoolToWord.java
@@ -0,0 +1,10 @@
+// Kata located at : https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    return b ? "Yes" : "No";
+  }
+  
+}
