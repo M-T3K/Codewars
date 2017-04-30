fdb604c68523dca19882ed06fc8c161182364926
M-T3K <kevinalb12@gmail.com>
2017-04-30 23:06:41 +0100

    Create BoolToString.cs

---
+++ 8-kyu/Boolean To String/BoolToString.cs
@@ -0,0 +1,13 @@
+// Kata located at : https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    if (word)  return "Yes";
+    else return "No";
+  }
+}
