50c792f225cda29e1636821d389df8a2ddb93bbf
M-T3K <kevinalb12@gmail.com>
2017-04-30 23:06:10 +0100

    Create BoolToString.cpp

---
+++ 8-kyu/Boolean To String/BoolToString.cpp
@@ -0,0 +1,9 @@
+// Kata located at : https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+using namespace std;
+
+string bool_to_word(bool value)
+{
+  if (value)  return "Yes";
+  return "No";
+}
