530f1fdb32f6f81baa0f2935c93871a9f85d0ab8
M-T3K <kevinalb12@gmail.com>
2017-04-30 16:23:03 +0100

    Added Link and M@@r

--- 7-kyu/Two Oldest Ages/twoOldestAges2.go
+++ 7-kyu/Two Oldest Ages/twoOldestAges2.go
@@ -1,3 +1,6 @@
+//Kata can be found at: https://www.codewars.com/kata/511f11d355fe575d2c000001
+//This time, I tried to use the built-in Go Functions to improve upon my first solution.
+
 package kata
 import "sort"
 
