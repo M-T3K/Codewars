0057ca7da9f8ae097b06c035f68c87d28bd4ce11
M-T3K <kevinalb12@gmail.com>
2017-04-30 22:58:57 +0100

    Create PressureOne.java

---
+++ 8-kyu/You Cant Code Under Pressure 1/PressureOne.java
@@ -0,0 +1,8 @@
+// Kata found at: https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+class Java {
+  public static int doubleInteger(int i) {
+    // Double the integer and return it!
+    return i * 2;
+  }
+}
