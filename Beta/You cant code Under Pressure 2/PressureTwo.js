3cd18f37cc0720bc3a7da20c0f494c69a02d4b1b
M-T3K <kevinalb12@gmail.com>
2017-04-30 22:38:56 +0100

    Create PressureTwo.js

---
+++ Beta/You cant code Under Pressure 2/PressureTwo.js
@@ -0,0 +1,8 @@
+// Kata located at: https://www.codewars.com/kata/5546ea9bddfc5c0c38000026
+function Counter() {
+  
+  var Counter = 0;  
+  this.check = function()  {return Counter;};
+  this.increment = function()  {Counter++;};
+
+};
