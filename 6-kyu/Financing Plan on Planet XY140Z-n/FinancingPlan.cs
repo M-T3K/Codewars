6b36eb93f6bbbb21f9cf5da39aebad82b9b656b4
M-T3K <kevinalb12@gmail.com>
2017-04-30 22:30:24 +0100

    Create FinancingPlan.cs

---
+++ 6-kyu/Financing Plan on Planet XY140Z-n/FinancingPlan.cs
@@ -0,0 +1,8 @@
+// Kata completed at: https://www.codewars.com/kata/559ce00b70041bc7b600013d
+public class Finance 
+{
+  public static ulong finance(ulong n) 
+  {        
+    return n * (n + 1) * (n + 2) / 2;
+  }
+}
