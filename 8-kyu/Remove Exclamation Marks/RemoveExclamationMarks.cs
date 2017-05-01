def40d26c547b33a2351d3a646d13b86cbc99019
M-T3K <kevinalb12@gmail.com>
2017-05-01 12:09:14 +0100

    Create RemoveExclamationMarks.cs

---
+++ 8-kyu/Remove Exclamation Marks/RemoveExclamationMarks.cs
@@ -0,0 +1,9 @@
+// Kata located at: https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s)
+  {
+    return s.Replace("!", "");
+  }
+}
