a26d923906397fe908dbcd77ee03af839fbc818c
M-T3K <kevinalb12@gmail.com>
2017-05-01 12:12:08 +0100

    Create VolumeCuboid.cpp

---
+++ 8-kyu/Volume of a Cuboid/VolumeCuboid.cpp
@@ -0,0 +1,6 @@
+// Kata located at: https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+double getVolumeOfCubiod(double length, double width, double height) {
+  // Your code here...
+  return length * width * height;
+}
