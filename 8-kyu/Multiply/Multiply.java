8329dcc04109c7eaf3d8be1afcf2512500ede9d6
M-T3K <kevinalb12@gmail.com>
2017-05-01 12:22:23 +0100

    Create Multiply.java

---
+++ 8-kyu/Multiply/Multiply.java
@@ -0,0 +1,8 @@
+// This was my entry test for Codewars.
+// This Kata can be found at: https://www.codewars.com/kata/50654ddff44f800200000004
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
