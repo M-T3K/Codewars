9f2447727332eba61b153b2f72c7ef7b52cf3fc3
M-T3K <kevinalb12@gmail.com>
2017-05-01 12:10:48 +0100

    Create ILoveYou.js

---
+++ 8-kyu/I Love You/ILoveYou.js
@@ -0,0 +1,11 @@
+// Kata can be solved here: https://www.codewars.com/kata/57f24e6a18e9fad8eb000296
+
+function howMuchILoveYou(nbPetals) {
+    // your code
+    if(nbPetals % 6 == 0)  return "not at all";
+    else if(nbPetals % 6 == 1) return "I love you";
+    else if(nbPetals % 6 == 2) return "a little";
+    else if(nbPetals % 6 == 3) return "a lot";
+    else if (nbPetals % 6 == 4) return "passionately";
+    else if (nbPetals % 6 == 5) return "madly";
+}
