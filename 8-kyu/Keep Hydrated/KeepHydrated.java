b0058f34304c2b5581e48d825ae99ab7418093a5
M-T3K <kevinalb12@gmail.com>
2017-05-01 12:04:25 +0100

    Create KeepHydrated.java

---
+++ 8-kyu/Keep Hydrated/KeepHydrated.java
@@ -0,0 +1,11 @@
+// Kata located at: https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+public class KeepHydrated  {
+
+  public int Liters(double time)  {
+    
+    return (int)(time / 2);
+  
+  }
+
+}
