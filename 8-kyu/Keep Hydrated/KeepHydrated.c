29433d2bbc357d15bb874d37eb47883a7c4fb670
M-T3K <kevinalb12@gmail.com>
2017-05-01 11:58:52 +0100

    Create KeepHydrated.c

---
+++ 8-kyu/Keep Hydrated/KeepHydrated.c
@@ -0,0 +1,9 @@
+// Kata located at: https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+#include <stdio.h>      
+#include <math.h>
+
+int Liters(double time) {
+  //The fun begins here.
+  return trunc(time*0.5);
+}
